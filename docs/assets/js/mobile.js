a80fcfc59f52bfe2c26ad653023ff37af09f4bb5
Harvey Zack <laozhaochaguan@gmail.com>
2019-05-29 15:31:56 +0800

    Update mobile.js

--- docs/assets/js/mobile.js
+++ docs/assets/js/mobile.js
@@ -1,7 +1,7 @@
 var vConsole = new VConsole();
 var url = 'https://zhw2590582.github.io/assets-cdn';
 var flv = new FlvPlayer({
-    url: './your-name-H264(Baseline@L52)-AAC.flv',
+    url: url + '/video/your-name-H264(Baseline@L52)-AAC.flv',
     container: document.querySelector('.flvplayer-app'),
     debug: true,
 });

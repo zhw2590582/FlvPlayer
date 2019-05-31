527ed99c464d6107c0ef967d6256e3f9397180d2
zhw2590582 <laozhaochaguan@gmail.com>
2019-06-01 00:56:30 +0800

    update demo

--- docs/assets/example/index.js
+++ docs/assets/example/index.js
@@ -1,7 +1,15 @@
 var url = 'https://zhw2590582.github.io/assets-cdn';
 var flv = new FlvPlayer({
-    url: './your-name-H264(Baseline@L52)-AAC.flv',
-    poster: url + '/image/your-name-poster.jpeg',
+    url: url + '/video/weathering-with-you-H264(Baseline@L52)-AAC.flv',
+    poster: url + '/image/weathering-with-you-poster.jpg',
     container: document.querySelector('.flvplayer-app'),
     debug: true,
+    live: false,
+    muted: false,
+    loop: false,
+    autoplay: false,
+    controls: true,
+    frameRate: 30,
+    width: 640,
+    height: 360,
 });
--- docs/assets/js/mobile.js
+++ docs/assets/js/mobile.js
@@ -1,8 +1,16 @@
 var vConsole = new VConsole();
 var url = 'https://zhw2590582.github.io/assets-cdn';
 var flv = new FlvPlayer({
-    url: url + '/video/your-name-H264(Baseline@L52)-AAC.flv',
-    poster: url + '/image/your-name-poster.jpeg',
+    url: url + '/video/weathering-with-you-H264(Baseline@L52)-AAC.flv',
+    poster: url + '/image/weathering-with-you-poster.jpg',
     container: document.querySelector('.flvplayer-app'),
     debug: true,
+    live: false,
+    muted: false,
+    loop: false,
+    autoplay: false,
+    controls: true,
+    frameRate: 30,
+    width: 640,
+    height: 360,
 });

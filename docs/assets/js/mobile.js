94b308ff8484e57ca4c091d50272b39a8d86f0f8
Harvey Zack <laozhaochaguan@gmail.com>
2019-05-31 17:32:46 +0800

    更新手机demo

--- docs/assets/js/mobile.js
+++ docs/assets/js/mobile.js
@@ -2,6 +2,7 @@ var vConsole = new VConsole();
 var url = 'https://zhw2590582.github.io/assets-cdn';
 var flv = new FlvPlayer({
     url: url + '/video/your-name-H264(Baseline@L52)-AAC.flv',
+    poster: url + '/image/your-name-poster.jpeg',
     container: document.querySelector('.flvplayer-app'),
     debug: true,
 });

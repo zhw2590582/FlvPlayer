8b89695a0648dd101b0a65186c7be4bac4bd8514
Harvey Zack <laozhaochaguan@gmail.com>
2019-06-05 10:06:38 +0800

    add tinyh264

--- docs/assets/example/index.js
+++ docs/assets/example/index.js
@@ -1,6 +1,6 @@
 var url = 'https://zhw2590582.github.io/assets-cdn';
 var flv = new FlvPlayer({
-    url: url + '/video/weathering-with-you-H264(Baseline@L52)-AAC.flv',
+    url: './weathering-with-you-H264(Baseline@L52)-AAC.flv',
     poster: url + '/image/weathering-with-you-poster.jpg',
     container: '.flvplayer-app',
     debug: true,
@@ -10,7 +10,6 @@ var flv = new FlvPlayer({
     controls: true,
     hasAudio: true,
     volume: 7,
-    frameRate: 30,
     width: 640,
     height: 360,
 });

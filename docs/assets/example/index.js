98cfc798259406cad0efcbe5be50ad45a195512a
zhw2590582 <laozhaochaguan@gmail.com>
2019-08-23 10:04:39 +0800

    Update index.js

--- docs/assets/example/index.js
+++ docs/assets/example/index.js
@@ -1,8 +1,10 @@
-var url = 'https://zhw2590582.github.io/assets-cdn';
+var assets = 'https://zhw2590582.github.io/assets-cdn';
 var flv = new FlvPlayer({
     container: '.flvplayer-app',
-    url: url + '/video/weathering-with-you-H264(Baseline@L52)-AAC.flv',
-    poster: url + '/image/weathering-with-you-poster.jpg',
+    poster: assets + '/image/weathering-with-you-poster.jpg',
+    url: assets + '/video/weathering-with-you-H264(Baseline@L52)-AAC.flv',
+    // url: assets + '/video/weathering-with-you-H264(High@L52)-AAC.flv',
+    // url: assets + '/video/weathering-with-you-H264(Main@L52)-AAC.flv',
     decoder: './uncompiled/flvplayer-decoder-baseline.js',
     // decoder: './uncompiled/flvplayer-decoder-multiple.js',
     debug: true,

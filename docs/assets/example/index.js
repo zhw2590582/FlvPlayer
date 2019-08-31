2a39e84566b16b5aa798c22d1e89731a55299569
zhw2590582 <laozhaochaguan@gmail.com>
2019-08-31 10:15:50 +0800

    Update index.js

--- docs/assets/example/index.js
+++ docs/assets/example/index.js
@@ -2,7 +2,6 @@ var assets = 'https://zhw2590582.github.io/assets-cdn';
 var flv = new FlvPlayer({
     container: '.flvplayer-app',
     poster: assets + '/image/weathering-with-you-poster.jpg',
-    // url: './weathering-with-you-H264(Baseline@L52)-AAC.flv',
     url: assets + '/video/weathering-with-you-H264(Baseline@L52)-AAC.flv',
     // url: assets + '/video/weathering-with-you-H264(High@L52)-AAC.flv',
     // url: assets + '/video/weathering-with-you-H264(Main@L52)-AAC.flv',
